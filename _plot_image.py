9af03077a9db0d7897961f44dec1fbac2747aa75
Jack <jack.sankey@mcgill.ca>
2014-09-01 10:11:57 -0400

    Added a branch just for wiki resources.

--- _plot_image.py
+++
@@ -1,5 +0,0 @@
-import _plotting_mess
-
-data          = _plotting_mess.image_data
-function      = _plotting_mess.image_function
-file          = _plotting_mess.image_file
--- _plot_parametric.py
+++
@@ -1,3 +0,0 @@
-import _plotting_mess
-
-function = _plotting_mess.parametric_function
