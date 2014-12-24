b9f7df6095bdb95d798e6f5d345b48dd714759c0
Andrew Jayich <jayich@gmail.com>
2014-12-24 03:22:11 -0800

    Changed name of "data" test code to match new API change.  Eliminated extra white space.  Added a Test class for testing the fitter.  Included data from Bevington in fixtures for testing the fitter against.

--- tests/_data_test.py
+++ tests/_data_test.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-
+Module for testing _data.py
 """
 
 import os  # Used for loading fixtures
 import numpy as _n
 import spinmob as sm
-_dt = sm._data_types
+_dt = sm._data
 
 import unittest as _ut
 from unittest import TestLoader as _TL
@@ -22,15 +22,12 @@ def test():
     """
 
     suite_databox  = _TL().loadTestsFromTestCase(Test_databox)
+    suite_fitter  = _TL().loadTestsFromTestCase(Test_fitter)
 
-    suites = [suite_databox]
-
+    suites = [suite_databox, suite_fitter]
     alltests = _ut.TestSuite(suites)
 
-
     _ut.TextTestRunner(verbosity=2).run(alltests)
-
-
 
 
 class Test_databox(_ut.TestCase):
@@ -43,20 +40,16 @@ class Test_databox(_ut.TestCase):
         """
         Load data
         """
-
         # Path to the spinmob module
         self.dt_path    = os.path.dirname(_dt.__file__)
-        if self.debug: print "self.dt_path =", self.dt_path
         self.module_path = self.dt_path.rpartition('spinmob')
         self.module_path = self.module_path[0]
-        if self.debug: print "self.module_path=", self.module_path
         
         self.fixtures_path = os.path.join('spinmob', 'tests', 'fixtures', 'data_types')
         midPath = os.path.join(self.module_path, self.fixtures_path)        
         
         filename = 'simple_xy_data.dat'
         self.data_path = os.path.join(midPath, filename)
-        if self.debug: print "self.data_path =", self.data_path
 
         # setup a default databox to be used for testing.
         self.databox = _dt.databox()
@@ -70,15 +63,10 @@ class Test_databox(_ut.TestCase):
         filename = 'headers_xy.dat'
         self.data_path3 = os.path.join(midPath, filename)
 
-        # This is not a good idea, as you get all the spinmob prints
-        #self.databoxDebug = _dt.databox(debug=True, delimiter=',    ')
-
 
     def tearDown(self):
         """
-        Cleanup procedure for the imageData and backgroundData
         """
-
         self.dt_path       = None
         self.module_path   = None
         self.data_path     = None
@@ -92,13 +80,7 @@ class Test_databox(_ut.TestCase):
         """
         val = self.databox.__repr__()
         exp = "\nDatabox Instance\n  header elements: 0\n  columns of data: 0\n"
-
-#        if self.debug:
-#            print "function output procedure path = ", val
-#            print "correct procedure path = ", out
         self.assertEqual(val, exp)
-
-
 
     def test_load_file(self):
         self.databox.load_file(path=self.data_path)
@@ -108,21 +90,17 @@ class Test_databox(_ut.TestCase):
         
         # The expected response
         exp = 85.0
-
         self.assertEqual(val, exp)
 
     def test_defaultDelimiter(self):
         val = self.databox.delimiter
         exp = None
-
         self.assertEqual(val, exp)        
 
     def test_CSVDelimiter(self):
         val = self.databoxCSV.delimiter
         exp = ', '
-        
         self.assertEqual(val, exp)        
-
 
     def test_load_fileCSV(self):
         """
@@ -135,13 +113,11 @@ class Test_databox(_ut.TestCase):
         
         # The expected response
         exp = 90.0
-
         self.assertEqual(val, exp)
 
 
     def test_pop_data_point(self):
-        self.databox.load_file(path=self.data_path)
-        
+        self.databox.load_file(path=self.data_path) 
         
         # Check a value of the loaded file, first level
         val = self.databox.pop_data_point(3, ckeys=[0])
@@ -165,41 +141,31 @@ class Test_databox(_ut.TestCase):
 
     def test___len__(self):
         self.databox.load_file(path=self.data_path)   
-        val = self.databox.__len__()
-        
+        val = self.databox.__len__() 
         exp = 2
         self.assertEqual(val, exp)
-        
         
     def test___setitem___str(self):
         self.databox.load_file(path=self.data_path)   
         self.databox.__setitem__(0, 'test_item')
-        
         val = self.databox[0]
-        
         exp = 'test_item'
         self.assertEqual(val, exp)
-
 
     def test___setitem___int(self):
         self.databox.load_file(path=self.data_path) 
         self.databox.__setitem__(2, [78, 87])
-        
         val = self.databox[2]
         val = val.tolist()
-        
         exp = [78, 87]
         self.assertListEqual(val, exp)        
-        
         
     def test___getslice__(self):
         self.databox.load_file(path=self.data_path) 
         val = self.databox.__getslice__(0, 1)
         val = val[0]
         val = val.tolist()
-
         val = val[0:5]   # Just check the first five elements
-        
         exp = [85.0, 90.0, 95.0, 100.0, 105.0] 
         self.assertListEqual(val, exp)            
         
@@ -209,20 +175,10 @@ class Test_databox(_ut.TestCase):
         
 
     def test_h_str(self):
-        self.databox.load_file(path=self.data_path3) 
-        
+        self.databox.load_file(path=self.data_path3)         
         val = self.databox.h('header1')
-
-
         exp = 'value1'
-        self.assertEqual(val, exp)  
-        
-        
-#    def test_get_XYZ(self):
-#        """
-#        Make up data for this test?
-#        """
-#        self.databox.load_file(path=self.data_path)
+        self.assertEqual(val, exp)          
 
     def test_h_None(self):
         """
@@ -231,14 +187,10 @@ class Test_databox(_ut.TestCase):
         TODO: possible better way of handling/collecting this error message
         while testing.
         """
-        self.databox.load_file(path=self.data_path3) 
-        
+        self.databox.load_file(path=self.data_path3)  
         val = self.databox.h()
-
-
         exp = None
         self.assertEqual(val, exp)          
-
 
     def test_h_Fragment(self):
         """
@@ -248,10 +200,7 @@ class Test_databox(_ut.TestCase):
         while testing.
         """
         self.databox.load_file(path=self.data_path3) 
-        
         val = self.databox.h('fragment')
-
-
         exp = None
         self.assertEqual(val, exp)          
 
@@ -263,35 +212,25 @@ class Test_databox(_ut.TestCase):
         while testing.
         """
         self.databox.load_file(path=self.data_path3) 
-        
         val = self.databox.h('header')
-
-
         exp = 'value1'
         self.assertEqual(val, exp)   
-
 
     def test_pop_column_ckey(self):
         self.databox.load_file(path=self.data_path3) 
         val = self.databox.pop_column('x_data')        
         val = val.tolist()
-
-        val = val[0:5]   # Just check the first five elements
-        
+        val = val[0:5]   # Just check the first five elements        
         exp = [85.0, 90.0, 95.0, 100.0, 105.0] 
         self.assertListEqual(val, exp) 
-
 
     def test_pop_column_int(self):
         self.databox.load_file(path=self.data_path3) 
         val = self.databox.pop_column(0)
         val = val.tolist()
-
-        val = val[0:5]   # Just check the first five elements
-        
+        val = val[0:5]   # Just check the first five elements        
         exp = [85.0, 90.0, 95.0, 100.0, 105.0] 
         self.assertListEqual(val, exp)       
-
 
     def test_pop_column_neg(self):
         """
@@ -302,12 +241,49 @@ class Test_databox(_ut.TestCase):
         self.databox.load_file(path=self.data_path3) 
         val = self.databox.pop_column(-2)
         val = val.tolist()
-
-        val = val[0:5]   # Just check the first five elements
-        
+        val = val[0:5]   # Just check the first five elements        
         exp = [85.0, 90.0, 95.0, 100.0, 105.0] 
         self.assertListEqual(val, exp)     
 
+
+
+class Test_fitter(_ut.TestCase):
+    """
+    Test class for fitter.
+    """
+    debug = False    
+    
+    def setUp(self):
+        """
+        Load data
+        """
+        # Path to the spinmob module
+        self.dt_path    = os.path.dirname(_dt.__file__)
+        self.module_path = self.dt_path.rpartition('spinmob')
+        self.module_path = self.module_path[0]
+        
+        self.fixtures_path = os.path.join('spinmob', 'tests', 'fixtures', '_data', 'fitter')
+        midPath = os.path.join(self.module_path, self.fixtures_path)        
+        
+        filename = 'Bevington Ex 7p1.dat'
+        self.data_path = os.path.join(midPath, filename)
+
+
+    def tearDown(self):
+        """
+        """
+
+        self.dt_path       = None
+        self.module_path   = None
+        self.data_path     = None
+        self.fixtures_path = None
+
+
+    def test_bevington_chi_squared(self):
+        """
+        Test against Example 7.1 in Bevington.
+        """
+        
 
 
 if __name__ == "__main__":
