f674225c9aeeb95294f847d8571db8c361eb60db
Andrew Jayich <jayich@gmail.com>
2015-06-15 20:29:54 -0700

    Changed ylabel of residuals plot to 'studentized residuals' from 'residuals'.
    Clarified the _residuals docstring.
    
    Got rid of some trailing white spaces.

--- _data.py
+++ _data.py
@@ -152,12 +152,12 @@ class databox:
 
         ##### read in the header information
         self.header_lines = []
-        
+
         for n in range(len(lines)):
-                        
+
             # split the line by the delimiter
             s = lines[n].strip().split(self.delimiter)
-            
+
             # remove a trailing whitespace entry if it exists.
             if len(s) and s[-1].strip() == '': s.pop(-1)
 
@@ -165,15 +165,15 @@ class databox:
             if first_data_line=="auto" and _s.fun.elements_are_numbers(s):
 
                 # we've reached the first data line
-                first_data_line = n                                
-                
+                first_data_line = n
+
                 # quit the header loop
                 break;
 
             ### now we know it's a header line
 
             # save the lines for the avid user.
-            self.header_lines.append(lines[n].strip())            
+            self.header_lines.append(lines[n].strip())
 
             # first thing to try is simply evaluating the remaining string
             try:
@@ -200,13 +200,13 @@ class databox:
 
         # special case: no header
         if first_data_line == 0: self.ckeys = []
-    
-        # start by assuming it's the previous line    
+
+        # start by assuming it's the previous line
         else: self.ckeys = lines[first_data_line-1].strip().split(self.delimiter)
 
         # count the number of actual data columns for comparison
         column_count = len(lines[first_data_line].strip().split(self.delimiter))
-        
+
         # check to see if ckeys is equal in length to the
         # number of data columns. If it isn't, it's a false ckeys line
         if len(self.ckeys) >= column_count:
@@ -227,16 +227,16 @@ class databox:
 
         # define a quick function to convert i's to j's
         def fix(x): return x.replace('i','j')
-        
+
         # loop over the remaining data lines, converting to numbers
-        z = _n.genfromtxt((fix(x) for x in lines[first_data_line:]), 
+        z = _n.genfromtxt((fix(x) for x in lines[first_data_line:]),
                           delimiter=self.delimiter,
                           dtype=complex)
 
         # fix for different behavior of genfromtxt on single columns
         if len(z.shape)==2: z = z.transpose()
         else:               z = [z]
-        
+
         # Add all the columns
         for n in range(len(self.ckeys)):
 
@@ -312,7 +312,7 @@ class databox:
         # loop over the columns and pop the data
         popped = []
         for k in self.ckeys:
-            
+
             # first convert to a list
             data = list(self.c(k))
 
@@ -326,38 +326,38 @@ class databox:
 
     def insert_data_point(self, new_data, index=None):
         """
-        Inserts a data point at index n. 
-        
+        Inserts a data point at index n.
+
         new_data    a list or array of new data points, one for each column.
         index       where to insert the point(s) in each column. None => append.
         """
-        
+
         if not len(new_data) == len(self.columns) and not len(self.columns)==0:
             print "ERROR: new_data must have as many elements as there are columns."
             return
-        
-        # otherwise, we just auto-add this data point as new columns        
+
+        # otherwise, we just auto-add this data point as new columns
         elif len(self.columns)==0:
             for i in range(len(new_data)): self[i] = [new_data[i]]
-    
-        # otherwise it matches length so just insert it.    
+
+        # otherwise it matches length so just insert it.
         else:
             for i in range(len(new_data)):
-                
+
                 # get the array and turn it into a list
                 data = list(self[i])
-    
+
                 # append or insert
                 if index == None: data.append(       new_data[i])
                 else:             data.insert(index, new_data[i])
-                
+
                 # reconvert to an array
                 self[i] = _n.array(data)
 
-    def append_data_point(self, new_data): 
+    def append_data_point(self, new_data):
         """
         Appends the supplied data point to the column(s).
-        
+
         new_data    a list or array of new data points, one for each column.
         """
         return self.insert_data_point(new_data)
@@ -628,7 +628,7 @@ class databox:
         """
         This will insert/overwrite a new column and fill it with the data from the
         the supplied array.
-        
+
         data_array  data; can be a list, but will be converted to numpy array
         ckey        name of the column; if an integer is supplied, uses self.ckeys[ckey]
         index       where to insert this column. None => append to end.
@@ -647,20 +647,20 @@ class databox:
         """
         This will append a new column and fill it with the data from the
         the supplied array.
-        
+
         data_array  data; can be a list, but will be converted to numpy array
         ckey        name of the column.
         """
-        if not type(ckey) == str: 
+        if not type(ckey) == str:
             print "ERROR: ckey should be a string!"
             return
 
-        if ckey in self.ckeys: 
+        if ckey in self.ckeys:
             print "ERROR: ckey '"+ckey+"' already exists!"
             return
-        
+
         self.insert_column(data_array, ckey)
-        
+
     def clear_columns(self):
         """
         This will remove all the ckeys and columns.
@@ -840,7 +840,7 @@ class fitter():
                               xscale        = 'linear', # axis scale type
                               yscale        = 'linear', # axis scale type
                               coarsen       = 1,        # how much to coarsen the data
-                              
+
                               # styles of plots
                               style_data   = dict(marker='+', color='b',   ls=''),
                               style_fit    = dict(marker='',  color='r',    ls='-'),
@@ -1100,16 +1100,16 @@ class fitter():
                             data matching the dimensionality of xdata and ydata
 
         Results will be stored in self.xdata, self.ydata, self.eydata
-        
+
         **kwargs are sent to set()
         """
-        
+
         # warn the user
         if eydata == None:
             print "\nWARNING: Setting eydata=None (i.e. the default) results in a random guess for the error bars associated with ydata. This will allow you to fit, but results in meaningless fit errors. Please estimate your errors and supply an argument such as:\n"
             print "  eydata = 0.1"
             print "  eydata = [[0.1,0.1,0.1,0.1,0.2],[1,1,1,1,1]]\n"
-        
+
         # make sure xdata and ydata are lists of data sets
         if not _s.fun.is_iterable(xdata[0]): xdata = [xdata]
         if not _s.fun.is_iterable(ydata[0]): ydata = [ydata]
@@ -1129,12 +1129,12 @@ class fitter():
         if not _s.fun.is_iterable(eydata): eydata = [eydata]*len(xdata)
 
         # example eydata at this point: [None, None], [3,3], [None,3] or [None, [1,2,1,2,1]]
-        
+
         # catch the potentially ambiguous case [3,3] where this could either be "two constants"
-        # or "the error bars for each data point" (assume the latter if possible)        
+        # or "the error bars for each data point" (assume the latter if possible)
         if _s.fun.elements_are_numbers(eydata) and len(eydata) == len(ydata[0]):
             eydata = [_n.array(eydata)]
-                
+
         # make sure the lengths match
         while len(eydata) < len(ydata): eydata.append(eydata[0])
 
@@ -1149,7 +1149,7 @@ class fitter():
                 eydata[n] = _n.ones(len(xdata[n])) * (max(ydata[n])-min(ydata[n]))/20.
 
             # use constant error bars
-            elif _s.fun.is_a_number(eydata[n]): 
+            elif _s.fun.is_a_number(eydata[n]):
                 eydata[n] = _n.ones(len(xdata[n])) * eydata[n]
 
             eydata[n] = _n.array(eydata[n]) * 1.0
@@ -1177,18 +1177,18 @@ class fitter():
 
     def set_guess_to_fit(self):
         """
-        If you have a fit result, set the guess parameters to the 
+        If you have a fit result, set the guess parameters to the
         fit parameters.
         """
-        if self.results == None: 
+        if self.results == None:
             print "No fit results to use! Run fit() first."
             return
-            
+
         # loop over the results and set the guess values
         for n in range(len(self._pguess)): self._pguess[n] = self.results[0][n]
-        
-        if self['autoplot']: self.plot()        
-        
+
+        if self['autoplot']: self.plot()
+
         return self
 
     def _massage_data(self):
@@ -1363,9 +1363,9 @@ class fitter():
 
     def _residuals(self, p=None):
         """
-        This function returns a list of vectors of the differences between the
-        model and ydata_massaged, scaled by the error. This function relies
-        on a previous call to set_data().
+        Returns a list of studentized residuals, (ydata_massaged - model)/error
+
+        This function relies on a previous call to set_data().
 
         p=None means use the fit results
         """
@@ -1530,8 +1530,8 @@ class fitter():
                                     _n.log10(max(self._xdata_massaged[n])),
                                     self['fpoints'][n], True, 10.0)
 
-                # otherwise do linear spacing                
-                else:                
+                # otherwise do linear spacing
+                else:
                     x = _n.linspace(min(self._xdata_massaged[n]),
                                     max(self._xdata_massaged[n]),
                                     self['fpoints'][n])
@@ -1601,7 +1601,7 @@ class fitter():
             else:                         _p.xlabel(self['xlabel'][n])
             if self['ylabel'][n] == None: _p.ylabel('ydata['+str(n)+']')
             else:                         _p.ylabel(self['ylabel'][n])
-            a1.set_ylabel('residuals')
+            a1.set_ylabel('studentized residuals')
 
             # Assemble the title
             wrap = 80
