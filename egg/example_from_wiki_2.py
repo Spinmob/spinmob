4536c2b3d9317484b56fa80e2f8a22de3d7ad6db
Jack Sankey <jack.sankey@gmail.com>
2015-10-16 20:43:00 -0400

    updated wiki example 2

--- egg/example_from_wiki_2.py
+++ egg/example_from_wiki_2.py
@@ -1,9 +1,18 @@
 from spinmob import egg
 
 # create the window and two tab areas
-w          = egg.gui.Window('Hey Guy II', autosettings_path='w.cfg')
-t_settings = w.place_object(egg.gui.TabArea())             .add_tab("Settings")
-t_raw_data = w.place_object(egg.gui.TabArea(), alignment=0).add_tab("Raw Data")
+w     = egg.gui.Window('Hey Guy II',   autosettings_path='w.cfg')
+tabs1 = w.place_object(egg.gui.TabArea(autosettings_path='tabs1.cfg'))
+tabs2 = w.place_object(egg.gui.TabArea(autosettings_path='tabs2.cfg'), alignment=0)
+
+# add some tabs
+t_settings  = tabs1.add_tab("Settings")
+t_raw       = tabs2.add_tab("Raw")
+t_processed = tabs2.add_tab("Processed")
+
+# load previous settings if any
+tabs1.load_gui_settings()
+tabs2.load_gui_settings()
 
 # show it!
 w.show()
@@ -14,39 +23,39 @@ w.show()
 ###########################
 # TREE DICTIONARY
 ###########################
-s = t_settings.place_object(egg.gui.TreeDictionary('s.cfg'))
+settings = t_settings.place_object(egg.gui.TreeDictionary('settings.cfg'))
 
 # add some settings
-s.add_parameter('DAQ/Rate',      1000,      type='float', step=1000, suffix='Hz', siPrefix=True, limits=[1,1e5])
-s.add_parameter('DAQ/Pants',        0,      type='list',  values=['required','optional'])
-s.add_parameter('Other/Function', 'cos(t)', type='str')
+settings.add_parameter('DAQ/Rate',      1000,      type='float', step=1000, suffix='Hz', siPrefix=True, limits=[1,1e5])
+settings.add_parameter('DAQ/Pants',        0,      type='list',  values=['required','optional'])
+settings.add_parameter('Other/Function', 'cos(t)', type='str')
 
 # load the previous settings if they exist.
-s.load()
+settings.load()
 
 # need to define this function to auto-save when settings change
-def s_changed(*a): s.save()
-s.connect_any_signal_changed(s_changed)
+def settings_changed(*a): settings.save()
+settings.connect_any_signal_changed(settings_changed)
 
 
 
 #############################
 # Databox Plotter
 #############################
-d = t_raw_data.place_object(egg.gui.DataboxPlot('*.raw', 'd.cfg'), alignment=0)
+d_raw = t_raw.place_object(egg.gui.DataboxPlot('*.raw', 'd_raw.cfg'), alignment=0)
 
 # Add some data / header info
-d['t']  = [1,2,3,4,5]
-d['V1'] = [1,2,1,2,1]
-d['V2'] = [2,1,2,1,2]
-d.h(Time="Pants O'Clock", Amp_Gain="32")
+d_raw['t']  = [1,2,3,4,5]
+d_raw['V1'] = [1,2,1,2,1]
+d_raw['V2'] = [2,1,2,1,2]
+d_raw.h(Time="Pants O'Clock", Amp_Gain="32")
 
 # Plot it
-d.plot()
+d_raw.plot()
 
 # Overload the post-data-load function to send header info to s
-def after_load_file(d): s.update(d.headers, ignore_errors=True)
-d.after_load_file = after_load_file
+def after_load_file(d): settings.update(d.headers, ignore_errors=True)
+d_raw.after_load_file = after_load_file
 
 
 
