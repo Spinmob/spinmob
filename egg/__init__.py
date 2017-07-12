609436cbaf4f193d7548965902e8635f0b634180
sankeylab <sankeylab@gmail.com>
2017-07-12 14:44:50 -0400

    Auto stash before merge of "master" and "origin/master"

--- egg/__init__.py
+++ egg/__init__.py
@@ -12,9 +12,8 @@ settings          = _spinmob.settings
 gui._settings     = settings
 dialogs._settings = settings
 
-print("\nWelcome to the Sankey Lab Easy Gui Generator!")
-print("  See example scripts for basic usage.\n")
-print("To learn more about pyqtgraph, use:")
+print("\nWelcome to the Sankey Lab Easy Gui Generator! See Spinmob Wiki and example scripts for basic usage. Note to IPython users: You may need to set the graphics backend to 'Qt5' prior to running.\n")
+print("To learn more about pyqtgraph widgets, use:")
 print("  >>> import pyqtgraph.examples")
 print("  >>> import pyqtgraph.examples.run()\n")
 
