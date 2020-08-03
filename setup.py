ab7c905823467a5b1e7bb71fe8941c6b896da07a
Jack Sankey <jack.sankey@gmail.com>
2020-08-03 13:43:18 -0400

    v3.4.9 passes win10

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-__version__ = '3.4.8' # Keep this on the first line so it's easy for __init__.py to grab.
+__version__ = '3.4.9' # Keep this on the first line so it's easy for __init__.py to grab.
 
 
 
