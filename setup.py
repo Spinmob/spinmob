d869fbd3a3d01f888e8640b12cdadef039d6f14e
Jack Sankey <jack.sankey@gmail.com>
2020-09-08 18:32:06 -0400

    v3.5.0 passes windows

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-__version__ = '3.4.15' # Keep this on the first line so it's easy for __init__.py to grab.
+__version__ = '3.5.0' # Keep this on the first line so it's easy for __init__.py to grab.
 
 
 
