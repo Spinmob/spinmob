81194828fd94422d615c92e0b2ed09f02b75d024
Jack <jack.sankey@gmail.com>
2021-03-23 12:28:34 -0400

    Spinmob v3.5.10 passes Linux

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-__version__ = '3.5.9' # Keep this on the first line so it's easy for __init__.py to grab.
+__version__ = '3.5.10' # Keep this on the first line so it's easy for __init__.py to grab.
 
 
 
