c737261f4613d23695b492b0b773726fc81d0524
Jack Sankey <jack.sankey@gmail.com>
2021-07-16 14:49:32 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-__version__ = '3.5.11' # Keep this on the first line so it's easy for __init__.py to grab.
+__version__ = '3.5.12' # Keep this on the first line so it's easy for __init__.py to grab.
 
 
 
