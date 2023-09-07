13e776e969604c0ec7894c9daa3079d64f74bdce
Jack Sankey <jack.sankey@gmail.com>
2023-09-07 13:55:48 -0400

    v3.5.18 passes linux

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-__version__ = '3.5.17' # Keep this on the first line so it's easy for __init__.py to grab.
+__version__ = '3.5.18' # Keep this on the first line so it's easy for __init__.py to grab.
 
 
 
